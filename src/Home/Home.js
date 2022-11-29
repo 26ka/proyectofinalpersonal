466e6d09d4b312813cbec1d8a423ef83d53f5e09
407 <407@academicos.cesde.edu.co>
2022-11-29 16:38:59 -0500

    se crea proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,60 @@
+import './Home.css'
+export function Home(){
+
+    return(
+        <>
+            <section>
+                <div class="banner">
+
+                </div>
+            </section>
+
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-8">
+                        <h2>jesse y joy</h2>
+                 <p>Jesse & Joy son un dúo de música pop originario de México, formado en 2005 por los hermanos Jesse (1982)​ y Joy Huerta Uecke (1986) en la Ciudad de México.
+                     ​​ El dúo ha lanzado seis álbumes de estudio, dos álbumes en vivo y un EP en Warner Music Latina.A principios de 2004, Jesse se consolidaba 
+                     como un talentoso jugador de baloncesto callejero en los suburbios de la ciudad de México. Un compañero se enteró de su música y le ofreció
+                      elaborar su logotipo. Para que los pudieran escuchar crearon su primera maqueta, que pasó de mano en mano hasta llegar a Warner Music México.
+                       Ni Jesse ni Joy sabían que habían generado interés en una internacional; ignorando esto, recibieron una llamada casi anónima en la cual les
+                        pedían una reunión al día siguiente en las oficinas de la discográfica. 
+                     Fueron y les ofrecieron firmar un contrato, pero pasó un año sin concretarse el acuerdo, tiempo que aprovecharon para seguir componiendo,
+                      conociendo músicos, productores y explorando sonidos.</p>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotykrl.appspot.com/o/jesse-y-joy-1-e1652476966361.jpg?alt=media&token=2f18a13c-f942-40d3-9ef7-579d2e31b820"></img>
+                        <div class="col-12 col-md-4">
+                        <i class="bi bi-vinyl-fill fs-1"></i>
+                        <i class="bi bi-vinyl-fill fs-1"></i> 
+                        <i class="bi bi-vinyl-fill fs-1"></i> 
+                        <i class="bi bi-vinyl-fill fs-1"></i> 
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                    
+            </section>
+            <section>
+                    <div class="container-fuid">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+                            <img scr="https://firebasestorage.googleapis.com/v0/b/spotykrl.appspot.com/o/2.jpg?alt=media&token=42729a30-e99c-4c07-8436-315f66a1ac03"alt=""class="img-fluid w-100"></img></div>
+                            <div class="col-12 col-md-4">
+                            <img scr="https://firebasestorage.googleapis.com/v0/b/spotykrl.appspot.com/o/2.jpg?alt=media&token=42729a30-e99c-4c07-8436-315f66a1ac03"alt=""class="img-fluid w-100"></img></div>
+                            <div class="col-12 col-md-4">
+                            <img scr="https://firebasestorage.googleapis.com/v0/b/spotykrl.appspot.com/o/2.jpg?alt=media&token=42729a30-e99c-4c07-8436-315f66a1ac03"alt=""class="img-fluid w-100"></img></div>
+                    </div>
+                    </div>
+                    
+          
+                        
+                    </section>
+            
+            
+          
+        </>
+    )
+
+}
+
+ 
